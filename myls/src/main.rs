6f9878cc6fdc5cc4e73043b735408959ee79d9cd
Marco Caamano <marco.caamano@gmail.com>
2024-09-29 15:47:02 -0600

    Parse ls colors env variable

--- myls/src/main.rs
+++ myls/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::path::Path;
@@ -19,6 +20,35 @@ fn get_current_path() -> String {
     }
 }
 
+fn parse_ls_colors() -> HashMap<String, String> {
+    let mut colors: HashMap<String, String> = HashMap::new();
+    let env_colors: String;
+    match env::var("LS_COLORS") {
+        Ok(data) => env_colors = data,
+        Err(_) => {
+            // just return an empty hashmap
+            return colors;
+        }
+    }
+    let values: Vec<&str> = env_colors.split(':').collect();
+
+    for value in values.into_iter() {
+        if value.is_empty() {
+            continue;
+        }
+        let item: Vec<&str> = value.split('=').collect();
+        if item.len() != 2 {
+            // did not get proper items, skip
+            continue;
+        }
+        let extension: String = item[0].replace("*.", "");
+        let color: &str = item[1];
+        colors.insert(extension, color.to_owned());
+    }
+
+    colors
+}
+
 /*
 * TODO
 * - create struct to represent each item
@@ -30,6 +60,12 @@ fn get_current_path() -> String {
 */
 
 fn main() {
+    let colors = parse_ls_colors();
+    println!("Found {} ls colors", &colors.len());
+    //for (extension, color) in colors.into_iter() {
+    //    println!("[{extension}] => [{color}]");
+    //}
+
     let cwd = get_current_path();
     println!("Current Path [{cwd}]");
     let path = Path::new(&cwd);
@@ -53,7 +89,6 @@ fn main() {
             dir_entry.file_type()
         );
     });
-    //for entry in fs::read_dir(path) {
-    //    let entry = entry;
-    //}
+
+    println!("Completed OK");
 }
