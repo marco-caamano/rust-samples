d640300e8e25c52a29ba9e49dc0daf2a26faf956
Marco Caamano <marco.caamano@gmail.com>
2024-10-02 23:55:53 -0600

    Add command line parsing for arguments, start with -a. Accept multiple paths to parse

--- myls/src/main.rs
+++ myls/src/main.rs
@@ -104,61 +104,88 @@ fn parse_file_entry(path: &Path) -> Result<FileItem, String> {
 
 fn main() {
     let mut my_files: Vec<FileItem> = Vec::new();
+    let mut paths_to_parse: Vec<String> = Vec::new();
+    let mut all_files: bool = false;
 
-    let args: Vec<String> = env::args().collect();
-    let target_path = if args.len() > 1 {
-        args[1].clone()
-    } else {
-        // we dont have a parameter so take the current directory
-        get_current_path()
-    };
-
-    let path = Path::new(&target_path);
-
-    if path.is_dir() {
-        let data = match fs::read_dir(&path) {
-            Ok(x) => x,
-            Err(e) => {
-                println!("ERROR: Failed to read directory ({:?})->({:?})", &path, e);
-                exit(1);
+    let mut skipped_first: bool = false;
+    for arg in env::args() {
+        if !skipped_first {
+            // skip the first argument that is the program name
+            skipped_first = true;
+            continue;
+        }
+        if arg.starts_with('-') {
+            // we have a group of command line parameters
+            if arg.contains('a') {
+                all_files = true;
             }
-        };
+        } else {
+            // then this is a path to parse
+            paths_to_parse.push(arg);
+        }
+    }
+    if paths_to_parse.is_empty() {
+        // since we did not get any paths to parse default to current directory
+        paths_to_parse.push(get_current_path());
+    }
 
-        data.for_each(|item| {
-            let dir_entry = item.unwrap_or_else(|e| {
-                println!("Got Empty data for entry {}", e);
-                exit(1);
+    for target_path in paths_to_parse.iter() {
+        let path = Path::new(target_path);
+        if path.is_dir() {
+            let data = match fs::read_dir(path) {
+                Ok(x) => x,
+                Err(e) => {
+                    println!("ERROR: Failed to read directory ({:?})->({:?})", path, e);
+                    exit(1);
+                }
+            };
+
+            data.for_each(|item| {
+                let dir_entry = item.unwrap_or_else(|e| {
+                    println!("Got Empty data for entry {}", e);
+                    exit(1);
+                });
+
+                let entry_path = dir_entry.path();
+                let my_path = entry_path.as_path();
+                let item = match parse_file_entry(my_path) {
+                    Ok(data) => data,
+                    Err(e) => {
+                        println!("ERROR: Failed to parse dir_entry[{}]", e);
+                        exit(1);
+                    }
+                };
+                my_files.push(item);
             });
-
-            let entry_path = dir_entry.path();
-            let my_path = entry_path.as_path();
-            let item = match parse_file_entry(my_path) {
+        } else {
+            let item = match parse_file_entry(path) {
                 Ok(data) => data,
                 Err(e) => {
-                    println!("ERROR: Failed to parse dir_entry[{}]", e);
+                    println!("ERROR: Failed to parse file ({})", e);
                     exit(1);
                 }
             };
             my_files.push(item);
-        });
-    } else {
-        let item = match parse_file_entry(path) {
-            Ok(data) => data,
-            Err(e) => {
-                println!("ERROR: Failed to parse file ({})", e);
-                exit(1);
-            }
-        };
-        my_files.push(item);
-    }
+        }
 
-    for item in &my_files {
-        println!(
-            "{}{} {}{}",
-            start_color(&item.color),
-            item.icon,
-            item.filename,
-            stop_color()
-        );
+        // Output the contents
+        if paths_to_parse.len() > 1 {
+            // we have multiple paths so prepend dir
+            println!("\n{}:", target_path);
+        }
+        for item in &my_files {
+            if item.filename.starts_with(".") && !all_files {
+                // skip hidden file
+                continue;
+            }
+            println!(
+                "{}{} {}{}",
+                start_color(&item.color),
+                item.icon,
+                item.filename,
+                stop_color()
+            );
+        }
+        my_files.clear();
     }
 }
