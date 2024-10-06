540d35664fad1eff8e8da8e6cca594d00ea0a6ce
Marco Caamano <marco.caamano@gmail.com>
2024-10-05 22:06:52 -0600

    cleanup, simplify and reorg code

--- myls/README.md
+++ myls/README.md
@@ -2,6 +2,5 @@
 
 Very basic implementation of ls command on rust. Used to practice rust, the std library and filesystem functions
 
-- Will add small icons to known filetypes to make it prettier
 - Will not implement all ls command line parameters, but the ones I find useful
 
--- myls/src/main.rs
+++ myls/src/main.rs
@@ -25,8 +25,8 @@ fn start_color(color: &str) -> String {
     let color = match Rgb::from_hex_str(color) {
         Ok(color) => color,
         Err(e) => {
-            println!("ERROR: failed to parse color ({})", e);
-            exit(1);
+            eprintln!("ERROR: failed to parse color {}. Error: {}", color, e);
+            Rgb::new(255.0, 255.0, 255.0, Some(255.0))
         }
     };
 
@@ -46,53 +46,54 @@ fn get_current_path() -> String {
     let path = match env::current_dir() {
         Ok(path) => path,
         Err(e) => {
-            println!("ERROR: failed to get current directory [{}]", e);
+            eprintln!("ERROR: failed to get current directory [{}]", e);
             exit(1);
         }
     };
     match path.to_str() {
         Some(cwd) => cwd.to_owned(),
         None => {
-            println!("ERROR: found invalid characters in path");
+            eprintln!("ERROR: found invalid characters in current path {:?}", path);
             exit(1);
         }
     }
 }
 
-fn parse_file_entry(path: &Path) -> Result<FileItem, String> {
-    let my_path = match path.to_str() {
-        Some(data) => data.to_string(),
-        None => return Err("Failed to parse path".to_string()),
-    };
+fn parse_file_entry(path: &String) -> Result<FileItem, String> {
+    let my_path = Path::new(path);
 
-    let my_filename = match path.file_name() {
-        Some(name) => match name.to_owned().into_string() {
-            Ok(fname) => fname,
-            Err(_) => return Err("Failed to parse filename".to_string()),
-        },
+    let os_filename = match my_path.file_name() {
+        Some(name) => name,
         None => return Err("Failed to parse filename".to_string()),
     };
 
-    let icon = icon_for_file(Path::new(&my_path), Some(Theme::Dark));
-    let my_icon = if path.is_dir() { '' } else { icon.icon };
-
-    let my_color = if path.is_dir() {
-        "#3483eb".to_string()
-    } else {
-        icon.color.to_string()
+    let my_filename = match os_filename.to_owned().into_string() {
+        Ok(fname) => fname,
+        Err(_) => return Err("Failed to parse filename".to_string()),
     };
 
+    let my_icon: char;
     let file_type: FileType;
-    if path.is_file() {
-        file_type = FileType::File;
-    } else if path.is_dir() {
+    let my_color: String;
+    if my_path.is_dir() {
         file_type = FileType::Dir;
+        my_icon = '';
+        my_color = "#3483eb".to_string();
+    } else if my_path.is_file() {
+        file_type = FileType::File;
+        let icon = icon_for_file(Path::new(&my_path), Some(Theme::Dark));
+        my_icon = icon.icon;
+        my_color = icon.color.to_string();
     } else {
+        // TODO: review the color or any other special handling for symlink
         file_type = FileType::Symlink;
+        let icon = icon_for_file(Path::new(&my_path), Some(Theme::Dark));
+        my_icon = icon.icon;
+        my_color = icon.color.to_string();
     }
 
     let item = FileItem {
-        path: my_path,
+        path: path.to_string(),
         filename: my_filename,
         filetype: file_type,
         color: my_color,
@@ -102,8 +103,57 @@ fn parse_file_entry(path: &Path) -> Result<FileItem, String> {
     Ok(item)
 }
 
-fn main() {
+fn parse_directory(path: &Path, items: &mut Vec<String>) {
+    let data = match fs::read_dir(path) {
+        Ok(x) => x,
+        Err(e) => {
+            eprintln!("ERROR: Failed to read directory ({:?})->({:?})", path, e);
+            // return empty vec
+            return;
+        }
+    };
+
+    data.for_each(|item| {
+        match item {
+            Ok(entry) => {
+                let entry_path = entry.path().to_owned();
+                if let Some(path) = entry_path.to_str() {
+                    items.push(path.to_string());
+                }
+            }
+            Err(e) => {
+                eprintln!("Error reading directory entry {}", e);
+            }
+        };
+    });
+}
+
+fn parse_path(target_path: &String) -> Vec<FileItem> {
+    let mut my_paths: Vec<String> = Vec::new();
     let mut my_files: Vec<FileItem> = Vec::new();
+
+    let path = Path::new(target_path);
+    if path.is_dir() {
+        parse_directory(path, &mut my_paths);
+    } else {
+        my_paths.push(target_path.to_string());
+    }
+
+    for path in my_paths.iter() {
+        let item = match parse_file_entry(path) {
+            Ok(data) => data,
+            Err(e) => {
+                eprintln!("ERROR: Failed to parse file ({})", e);
+                exit(1);
+            }
+        };
+        my_files.push(item);
+    }
+
+    my_files
+}
+
+fn main() {
     let mut paths_to_parse: Vec<String> = Vec::new();
     let mut all_files: bool = false;
 
@@ -125,7 +175,7 @@ fn main() {
             if path.exists() {
                 paths_to_parse.push(arg);
             } else {
-                println!("Cannot access [{}] (path does not exist)", arg);
+                eprintln!("Cannot access [{}] (path does not exist)", arg);
             }
         }
     }
@@ -136,43 +186,7 @@ fn main() {
 
     let mut add_path_separator = false;
     for target_path in paths_to_parse.iter() {
-        let path = Path::new(target_path);
-        if path.is_dir() {
-            let data = match fs::read_dir(path) {
-                Ok(x) => x,
-                Err(e) => {
-                    println!("ERROR: Failed to read directory ({:?})->({:?})", path, e);
-                    exit(1);
-                }
-            };
-
-            data.for_each(|item| {
-                let dir_entry = item.unwrap_or_else(|e| {
-                    println!("Got Empty data for entry {}", e);
-                    exit(1);
-                });
-
-                let entry_path = dir_entry.path();
-                let my_path = entry_path.as_path();
-                let item = match parse_file_entry(my_path) {
-                    Ok(data) => data,
-                    Err(e) => {
-                        println!("ERROR: Failed to parse dir_entry[{}]", e);
-                        exit(1);
-                    }
-                };
-                my_files.push(item);
-            });
-        } else {
-            let item = match parse_file_entry(path) {
-                Ok(data) => data,
-                Err(e) => {
-                    println!("ERROR: Failed to parse file ({})", e);
-                    exit(1);
-                }
-            };
-            my_files.push(item);
-        }
+        let my_files: Vec<FileItem> = parse_path(target_path);
 
         // Output the contents
         if paths_to_parse.len() > 1 {
@@ -196,6 +210,5 @@ fn main() {
                 stop_color()
             );
         }
-        my_files.clear();
     }
 }
