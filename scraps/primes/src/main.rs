9b92fe281ebfbc96577ab4d6aa21a1322451b2c4
Marco Caamano <marco.caamano@gmail.com>
2025-02-23 16:09:22 -0600

    Track and report some execution stats

--- scraps/primes/src/main.rs
+++ scraps/primes/src/main.rs
@@ -1,4 +1,9 @@
 use clap::Parser;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::process::exit;
+use std::time::Instant;
 
 /// Calculate Prime Numbers from a range
 #[derive(Parser)]
@@ -13,29 +18,121 @@ struct Args {
     /// Enable verbose output
     #[arg(short, long)]
     verbose: bool,
+
+    /// Write stats to output file
+    #[arg(short, long)]
+    output: Option<String>,
+}
+
+struct Context {
+    start: u32,
+    end: u32,
+    verbose: bool,
+    has_output: bool,
+    filename: String,
+}
+
+impl Context {
+    fn new() -> Context {
+        Context {
+            start: 0,
+            end: 0,
+            verbose: false,
+            has_output: false,
+            filename: String::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Item {
+    number: u32,
+    is_prime: bool,
+    duration: std::time::Duration,
 }
 
 fn main() {
+    let mut ctx = Context::new();
     let args = Args::parse();
-    let mut start: u32 = args.start;
-    let mut end: u32 = args.end;
+    ctx.start = args.start;
+    ctx.end = args.end;
+    ctx.verbose = args.verbose;
+
+    if Option::is_some(&args.output) {
+        ctx.has_output = true;
+        ctx.filename = args.output.unwrap();
+    }
 
     // sanity checks
-    if end < start {
+    if ctx.end < ctx.start {
         println!("ERROR: End is smaller than start, assuming inverted range");
-        std::mem::swap(&mut start, &mut end);
+        std::mem::swap(&mut ctx.start, &mut ctx.end);
     }
-    if start == 0 {
+    if ctx.start == 0 {
         println!("ERROR: Invalid Start at 0, assuming 1 instead");
-        start = 1;
+        ctx.start = 1;
+    }
+    if ctx.has_output && Path::new(&ctx.filename).exists() {
+        println!(
+            "ERROR: Output file [{}] already exists, will not overwrite file, aborting",
+            ctx.filename
+        );
+        exit(1);
     }
 
-    println!("Start           {}", start);
-    println!("End             {}", end);
-    println!("Verbose mode:   {:?}", args.verbose);
+    println!("Start           {}", ctx.start);
+    println!("End             {}", ctx.end);
+    println!("Verbose mode:   {:?}", ctx.verbose);
+    if ctx.has_output {
+        println!("Output file:    {}", &ctx.filename);
+    }
 
-    let result: Vec<u32> = calculate_primes(start, end, args.verbose);
-    println!("Result: {:?}", result);
+    let start = Instant::now();
+    let results: Vec<Item> = calculate_primes(&ctx);
+    let duration = start.elapsed();
+    println!("Primes:");
+    for item in results.iter() {
+        if item.is_prime {
+            if ctx.verbose {
+                println!("Number: {} duration: {:?} ", item.number, item.duration);
+            } else {
+                print!("{}, ", item.number);
+            }
+        }
+    }
+    if !ctx.verbose {
+        println!();
+    }
+    println!(
+        "Execution Took: [{}] seconds | [{}] milliseconds",
+        duration.as_secs(),
+        duration.as_millis()
+    );
+
+    if ctx.has_output {
+        let path = Path::new(&ctx.filename);
+        let mut file: File = match File::create(path) {
+            Ok(file) => file,
+            Err(e) => {
+                println!(
+                    "ERROR: Failed to open output file [{}] reason: {}",
+                    &ctx.filename, e,
+                );
+                exit(1);
+            }
+        };
+        for item in results.iter() {
+            if item.is_prime {
+                match writeln!(file, "{},{}", &item.number, &item.duration.as_nanos()) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("ERROR: Write failed [{}], aborting", e);
+                        exit(1);
+                    }
+                }
+            }
+        }
+    }
 }
 
 fn is_prime(num: u32) -> bool {
@@ -55,19 +152,24 @@ fn is_prime(num: u32) -> bool {
     true
 }
 
-fn calculate_primes(start: u32, end: u32, verbose: bool) -> Vec<u32> {
-    let mut result: Vec<u32> = Vec::new();
-    for i in start..=end {
-        if verbose {
-            println!("Testing i {}", i);
+fn calculate_primes(ctx: &Context) -> Vec<Item> {
+    let mut result: Vec<Item> = Vec::new();
+    for num in ctx.start..=ctx.end {
+        if ctx.verbose {
+            println!("Testing num {}", num);
         }
-        if is_prime(i) {
-            result.push(i);
-            if verbose {
-                println!("i {} is prime", i);
-            }
-        } else if verbose {
-            println!("i {} is NOT prime", i);
+        let start = Instant::now();
+        let num_is_prime = is_prime(num);
+        let item = Item {
+            number: num,
+            is_prime: num_is_prime,
+            duration: start.elapsed(),
+        };
+        result.push(item);
+        if ctx.verbose && num_is_prime {
+            println!("Number [{}] is prime", num);
+        } else if ctx.verbose {
+            println!("Number [{}] is NOT prime", num);
         }
     }
 
